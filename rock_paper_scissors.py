aa88b58324fdf064b69c1a9c0355b5e53cd544bc
Givemethecharts <cardamon6070@gmail.com>
2023-01-24 01:41:49 +0200

    colors_added
    we added different colors to our output text on the console

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,4 +1,14 @@
 import random
+def prRed(skk): print("\033[91m {}\033[00m".format(skk))
+def prGreen(skk): print("\033[92m {}\033[00m".format(skk))
+def prYellow(skk): print("\033[93m {}\033[00m".format(skk))
+def prLightPurple(skk): print("\033[94m {}\033[00m".format(skk))
+def prPurple(skk): print("\033[95m {}\033[00m".format(skk))
+def prCyan(skk): print("\033[96m {}\033[00m".format(skk))
+def prLightGray(skk): print("\033[97m {}\033[00m".format(skk))
+def prBlack(skk): print("\033[98m {}\033[00m".format(skk))
+
+
 rock = "Rock"
 paper = "Paper"
 scissors = "Scissors"
@@ -24,14 +34,14 @@ elif computer_random_number == 2:
 elif computer_random_number == 3:
     computer_move = scissors
 
-print(f"The computer chose {computer_move}")
+prPurple(f"The computer chose {computer_move}")
 
 if (our_option == rock and computer_move == scissors) or (our_option == paper and computer_move == rock) or (our_option == scissors and computer_move == paper):
-    print("You win!")
+    prCyan("You win!")
 elif our_option == rock and computer_move == rock or our_option == paper and computer_move == paper or our_option == scissors and computer_move == scissors:
-    print("Draw")
+    prYellow("Draw")
 else:
-    print("You lose!")
+    prRed("You lose!")
 
 
 
